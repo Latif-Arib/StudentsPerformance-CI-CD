5d17193a7dee9153713815cc4eb5777635ee4701
labdul749@gmail.com <labdul749@gmail.com>
2024-11-19 12:53:25 +0530

    changed the app from fastapi to Fask

--- application.py
+++ application.py
@@ -1,61 +1,53 @@
-from fastapi import FastAPI, Form, Request
-from pydantic import BaseModel
-from fastapi.templating import Jinja2Templates
-from fastapi.staticfiles import StaticFiles
-from fastapi.responses import HTMLResponse
-from typing import Optional, List
+from flask import Flask, render_template, request, jsonify
+from typing import List
 from src.pipeline.predict_pipeline import Prediction
 
-application = FastAPI()
+application = Flask(__name__)
+application.static_folder = 'static'
 
-application.mount("/static", StaticFiles(directory="static"), name="static")
+class StudentData:
+    def __init__(self, data_dict):
+        self.gender = data_dict['gender']
+        self.race_ethnicity = data_dict['race_ethnicity']
+        self.parental_level_of_education = data_dict['parental_level_of_education']
+        self.lunch = data_dict['lunch']
+        self.test_preparation_course = data_dict['test_preparation_course']
+        self.reading_score = [int(score) for score in data_dict['reading_score']]
+        self.writing_score = [int(score) for score in data_dict['writing_score']]
+    
+    def dict(self):
+        return {
+            'gender': self.gender,
+            'race_ethnicity': self.race_ethnicity,
+            'parental_level_of_education': self.parental_level_of_education,
+            'lunch': self.lunch,
+            'test_preparation_course': self.test_preparation_course,
+            'reading_score': self.reading_score,
+            'writing_score': self.writing_score
+        }
 
-templates = Jinja2Templates(directory='templates')
+@application.route('/', methods=['GET'])
+def get_form():
+    return render_template('index.html')
 
-class StudentData(BaseModel):
-    gender: List[str]
-    race_ethnicity: List[str]
-    parental_level_of_education: List[str]
-    lunch: List[str]
-    test_preparation_course: List[str]
-    reading_score: List[int]
-    writing_score: List[int]
-
-@application.get('/')
-async def get_form(request: Request):
-    return templates.TemplateResponse('index.html',{'request':request})
-
-
-@application.post("/submit/")
-async def submit_form(
-    request: Request,
-    gender: list[str] = Form(...),
-    race_ethnicity: list[str] = Form(...),
-    parental_level_of_education: list[str] = Form(...),
-    lunch: list[str] = Form(...),
-    test_preparation_course: list[str] = Form(...),
-    reading_score: list[int] = Form(...),
-    writing_score: list[int] = Form(...)
-):
-    data = StudentData(
-        gender=gender,
-        race_ethnicity=race_ethnicity,
-        parental_level_of_education=parental_level_of_education,
-        lunch=lunch,
-        test_preparation_course=test_preparation_course,
-        reading_score=reading_score,
-        writing_score=writing_score
-    )
+@application.route('/submit/', methods=['POST'])
+def submit_form():
+    # In Flask, request.form.getlist() is used to get multiple values for the same field
+    data = StudentData({
+        'gender': request.form.getlist('gender'),
+        'race_ethnicity': request.form.getlist('race_ethnicity'),
+        'parental_level_of_education': request.form.getlist('parental_level_of_education'),
+        'lunch': request.form.getlist('lunch'),
+        'test_preparation_course': request.form.getlist('test_preparation_course'),
+        'reading_score': request.form.getlist('reading_score'),
+        'writing_score': request.form.getlist('writing_score')
+    })
     
     model = Prediction()
     df = model.make_df(data.dict())
     prediction = model.predict(df)
     
-    
-    # return {"message": "Form submitted successfully!", "prediction": prediction[0]}
-    return templates.TemplateResponse('index.html',{
-        'request':request,
-        'prediction': int(prediction[0])
-    })
+    return render_template('index.html', prediction=int(prediction[0]))
 
-
+if __name__ == '__main__':
+    application.run(debug=True)
